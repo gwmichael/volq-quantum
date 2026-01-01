495bbae9140a065ae5f8d26c8feec46ff5606cf0
Michael Goodwin <michael.cgdwn@gmail.com>
2026-01-01 19:22:00 +0000

    Fix GATE_TO_MATRIX being a visible enum member in backend/single_qubit_gates

--- volq/backend/single_qubit_gates.py
+++ volq/backend/single_qubit_gates.py
@@ -8,7 +8,7 @@ class Gate(enum.Enum):
     Y = "Y"  # Pauli Y
     Z = "Z"  # Pauli Z
 
-    GATE_TO_MATRIX = {
+    _GATE_TO_MATRIX = {
         "I": np.array([[1,0],[0,1]], dtype = complex),
         "H": (1 / np.sqrt(2)) * np.array([[1, 1], [1, -1]], dtype = complex),
         "X": np.array([[0,1],[1,0]], dtype = complex),
@@ -17,4 +17,4 @@ class Gate(enum.Enum):
     }
 
     def matrix(self):
-        return self.GATE_TO_MATRIX[self.value]
+        return self._GATE_TO_MATRIX[self.value]
