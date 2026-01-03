b3bfe7b237a3938eb7ecc5afe81c4f8b46c549d2
Michael Goodwin <michael.cgdwn@gmail.com>
2026-01-03 23:32:50 +0000

    Add qubits, operator_cache, hardware_mode and syntax_validation in backend/context

--- volq/backend/context.py
+++ volq/backend/context.py
@@ -1,4 +1,39 @@
 class Context:
 
-    def __init__(self):
-        pass
+    def __init__(
+            self,
+            qubits,
+            operator_cache,
+            hardware_mode,
+            syntax_validation
+        ):
+        
+        self._qubits = qubits # Qubits has no setter by design
+        self._operator_cache_enabled = operator_cache
+        self._operator_cache = {}
+        self._hardware_mode = hardware_mode
+        self._syntax_validation_enabled = syntax_validation
+
+
+    def get_qubits(self):
+        return self._qubits
+    
+
+    def operator_cache_enabled(self):
+        return self._operator_cache_enabled
+
+
+    def toggle_operator_cache(self):
+        self._operator_cache_enabled = not self._operator_cache_enabled
+
+
+    def is_operator_cached(self, operator):
+        return True if operator in self._operator_cache else False
+    
+
+    def hardware_mode(self):
+        return self._hardware_mode
+    
+
+    def syntax_validation_enabled(self):
+        return self._syntax_validation_enabled
